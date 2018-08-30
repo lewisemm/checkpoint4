a876694cddae1137564bea72f906746f6a93cac4
Lewis Kabui <lewis.maina@andela.com>
2018-08-30 17:37:25 +0300

    (chore) Extract database information from environment

--- vistagrid/settings/base_settings.py
+++ vistagrid/settings/base_settings.py
@@ -91,10 +91,7 @@ WSGI_APPLICATION = 'vistagrid.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-  }
+  'default': dj_database_url.config()
 }
 
 
--- vistagrid/settings/prod_settings.py
+++ vistagrid/settings/prod_settings.py
@@ -2,5 +2,3 @@ from vistagrid.settings.base_settings import *
 
 DEBUG = False
 ALLOWED_HOSTS = ['*']
-
-DATABASES['default'] = dj_database_url.config()
